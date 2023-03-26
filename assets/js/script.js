635cdc6494425d07c2c2fc4fe3fc2f9b44e0dcf5
mykki92 <mike2108@hotmail.co.uk>
2023-03-26 20:03:06 +0000

    Write and test drag and drop javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     
 })
+
+function allowDrop(event) {
+    event.preventDefault();
+  }
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
