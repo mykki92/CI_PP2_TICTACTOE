789438654679fefdae0d1d4fde044ab4d9a26ead
mykki92 <mike2108@hotmail.co.uk>
2023-04-02 00:54:01 +0000

    Add javascript to lock the board while matches are checked and prevent the same card being selected twice

--- assets/js/memgame.js
+++ assets/js/memgame.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.game-card');
 
+//card flip
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard () {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -13,11 +17,11 @@ function flipCard () {
    }
 
    secondCard = this;
-   hasFlippedCard = false;
 
    checkForMatch();
 }
 
+//check for matching cards referencing their data frameworks
 function checkForMatch() {
    if (firstCard.dataset.framework === secondCard.dataset.framework) {
      disableCards();
@@ -30,14 +34,24 @@ function checkForMatch() {
 function disableCards() {
    firstCard.removeEventListener('click', flipCard);
    secondCard.removeEventListener('click', flipCard);
+   resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
    setTimeout(() => {
      firstCard.classList.remove('flip');
      secondCard.classList.remove('flip');
+
+     resetBoard();
    }, 1500);
-}
+ }
+
+ function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
 
 
 cards.forEach(card => card.addEventListener('click', flipCard));
